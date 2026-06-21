7418b9d110a630b410ae605971fbfc4b3b4aea3d
Arun Arun <arunbelursan1995@gmail.com>
2026-06-21 05:02:34 +0200

    Rename resource group and update storage references

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
 resource "azurerm_resource_group" "rg1" {
-  name     = "rg-log-demo"
+  name     = "rg-log-demo1"
   location = "East US"
 }
 
 resource "azurerm_storage_account" "sa1" {
   name                     = "arunlogstorage1"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rg1.name
+  location                 = azurerm_resource_group.rg1.location
 
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
 resource "azurerm_storage_container" "logs1" {
-  name                  = "applicationlogs"
-  storage_account_id    = azurerm_storage_account.sa.id
+  name                  = "applicationlogs1"
+  storage_account_id    = azurerm_storage_account.sa1.id
 
   container_access_type = "private"
 }
