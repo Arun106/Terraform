82933019b4982b82319bf85b218876066887eb63
Arun Arun <arunbelursan1995@gmail.com>
2026-06-21 04:37:44 +0200

    Rename resources in main.tf for clarity

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "rg1" {
   name     = "rg-log-demo"
   location = "East US"
 }
 
-resource "azurerm_storage_account" "sa" {
-  name                     = "arunlogstorage12345"
+resource "azurerm_storage_account" "sa1" {
+  name                     = "arunlogstorage1"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
 
@@ -12,7 +12,7 @@ resource "azurerm_storage_account" "sa" {
   account_replication_type = "LRS"
 }
 
-resource "azurerm_storage_container" "logs" {
+resource "azurerm_storage_container" "logs1 {
   name                  = "applicationlogs"
   storage_account_id    = azurerm_storage_account.sa.id
 
