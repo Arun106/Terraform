aa7ee31df50840750d591ce3ff743a40e8fcc4b7
Arun Arun <arunbelursan1995@gmail.com>
2026-06-29 08:56:40 +0200

    Refactor Terraform file and update PostgreSQL server
    Removed unused resources and updated PostgreSQL server configuration.

--- Terraform/Arun/TF_Managed_Disk_Fileshare.tf
+++ Terraform/Arun/TF_Managed_Disk_Fileshare.tf
@@ -1,91 +1,12 @@
-import {
-  to = azurerm_resource_group.rg3
-  id = "/subscriptions/e377d444-6055-48ff-b11c-588edaf7da68/resourceGroups/arun-rg"
-}
-
-resource "azurerm_resource_group" "rg3" {
-  name     = "arun-rg"
-  location = "East US"
-}
-
-resource "azurerm_managed_disk" "disk" {
-  name                 = "arun-disk"
-  location             = azurerm_resource_group.rg3.location
-  resource_group_name  = azurerm_resource_group.rg3.name
-  storage_account_type = "Standard_LRS"
-  create_option        = "Empty"
-  disk_size_gb         = 32
-}
-
-resource "azurerm_storage_account" "sa" {
-  name                     = "arunstorage123456"
-  resource_group_name      = azurerm_resource_group.rg3.name
-  location                 = azurerm_resource_group.rg3.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_storage_share" "fileshare" {
-  name               = "sharedfiles"
-  storage_account_id = azurerm_storage_account.sa.id
-  quota              = 50
-}
-
-resource "azurerm_virtual_network" "vnet" {
-  name                = "arun-vnet"
-  location            = azurerm_resource_group.rg3.location
-  resource_group_name = azurerm_resource_group.rg3.name
-  address_space       = ["10.0.0.0/16"]
-}
-
-resource "azurerm_subnet" "subnet" {
-  name                 = "arun-subnet"
-  resource_group_name  = azurerm_resource_group.rg3.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_network_interface" "nic" {
-  name                = "arun-nic"
-  location            = azurerm_resource_group.rg3.location
-  resource_group_name = azurerm_resource_group.rg3.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "arun-vm"
-  location            = azurerm_resource_group.rg3.location
-  resource_group_name = azurerm_resource_group.rg3.name
-  size                = "Standard_B1s"
-
-  admin_username                  = "azureuser"
-  admin_password                  = "azureuser@123"
-  disable_password_authentication = false
-
-  network_interface_ids = [azurerm_network_interface.nic.id]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
+resource "azurerm_resource_group" "pg_rg" {
+  name     = "arun-postgres-rg"
+  location = "Japan East"
 }
 
 resource "azurerm_postgresql_flexible_server" "postgres" {
-  name                = "arun-postgres-server123"
-  resource_group_name = azurerm_resource_group.rg3.name
-  location            = azurerm_resource_group.rg3.location
+  name                = "arun-postgres-japan2026"
+  resource_group_name = azurerm_resource_group.pg_rg.name
+  location            = azurerm_resource_group.pg_rg.location
 
   administrator_login    = "pgadminuser"
   administrator_password = "Postgres@12345"
@@ -109,14 +30,6 @@ resource "azurerm_postgresql_flexible_server_firewall_rule" "allow_my_ip" {
   server_id        = azurerm_postgresql_flexible_server.postgres.id
   start_ip_address = "49.37.162.6"
   end_ip_address   = "49.37.162.6"
-}
-
-output "disk_id" {
-  value = azurerm_managed_disk.disk.id
-}
-
-output "fileshare_name" {
-  value = azurerm_storage_share.fileshare.name
 }
 
 output "postgres_host" {
